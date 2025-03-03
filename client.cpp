0a4ee4543096167e5ec6555d17e9fb38ac8b210f
Eshunned <esinha543@gmail.com>
2025-03-03 18:50:40 +0000

    works

--- client.cpp
+++ client.cpp
@@ -1,63 +1,87 @@
 #include <iostream>
-#include <string>
 #include <thread>
-#include <arpa/inet.h>
 #include <unistd.h>
 #include <cstring>
+#include <arpa/inet.h>
 
 #define SERVER_IP "127.0.0.1"
-#define PORT 9090
+#define SERVER_PORT 9090
 #define BUFFER_SIZE 1024
 
-// Function to receive messages from server
-void receive_messages(int client_socket) {
+void receive_messages(int socket_fd) {
     char buffer[BUFFER_SIZE];
+    int bytes_received;
+
     while (true) {
-        memset(buffer, 0, BUFFER_SIZE);
-        int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
-        if (bytes_received <= 0) {
-            std::cerr << "Disconnected from server\n";
+        bytes_received = recv(socket_fd, buffer, sizeof(buffer), 0);
+        if (bytes_received == 0) {
+            std::cout << "Server disconnected." << std::endl;
             break;
         }
-        std::cout << "\n" << buffer << "\n> ";
-        std::cout.flush();
+        if (bytes_received < 0) {
+            std::cerr << "Error receiving message: " << strerror(errno) << std::endl;
+            break;
+        }
+
+        buffer[bytes_received] = '\0';  // Null-terminate the string
+        std::cout << "Message from server: " << buffer << std::endl;
     }
 }
 
 int main() {
-    int client_socket;
-    sockaddr_in server_addr{};
+    int socket_fd;
+    struct sockaddr_in server_addr;
 
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        std::cerr << "Socket creation failed\n";
+    // Create socket
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket failed");
         return -1;
     }
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
-
-    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        std::cerr << "Connection to server failed\n";
+    server_addr.sin_port = htons(SERVER_PORT);
+    
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address");
         return -1;
     }
 
-    std::cout << "Connected to the chat server\n> ";
-
-    std::thread receiver(receive_messages, client_socket);
-    receiver.detach();
-
-    std::string message;
-    while (true) {
-        std::cout << "> ";
-        std::getline(std::cin, message);
-        if (message == "exit") {
-            break;
-        }
-        send(client_socket, message.c_str(), message.size(), 0);
+    // Connect to server
+    if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        return -1;
     }
 
-    close(client_socket);
+    std::cout << "Connected to server " << SERVER_IP << " on port " << SERVER_PORT << std::endl;
+
+    std::string username;
+    std::cout << "Enter your username: ";
+    std::getline(std::cin, username);
+
+    // Start receiving messages in a separate thread
+    std::thread(receive_messages, socket_fd).detach();
+
+    char buffer[BUFFER_SIZE];
+
+    while (true) {
+        std::string message;
+        std::cout << "You: ";
+        std::getline(std::cin, message);
+
+        if (message == "exit") {
+            std::cout << "Disconnecting..." << std::endl;
+            break;
+        }
+
+        // Prepend username to the message
+        std::string message_to_send = username + ": " + message;
+
+        // Send the message
+        if (send(socket_fd, message_to_send.c_str(), message_to_send.length(), 0) == -1) {
+            std::cerr << "Error sending message: " << strerror(errno) << std::endl;
+        }
+    }
+
+    close(socket_fd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,94 +1,94 @@
 #include <iostream>
-#include <string>
+#include <unistd.h>
+#include <cstring>
 #include <vector>
 #include <thread>
-#include <mutex>
+#include <algorithm>  // Added to resolve std::remove issue
+#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <cstring>  // <-- Fixes 'memset' error
-#include <algorithm> // <-- Fixes 'remove' error
 
 #define PORT 9090
 #define BUFFER_SIZE 1024
 
 std::vector<int> clients;
-std::mutex clients_mutex;
 
-// Function to broadcast messages to all clients
-void broadcast(const std::string &message, int sender_fd) {
-    std::lock_guard<std::mutex> lock(clients_mutex);
-    for (int client_fd : clients) {
-        if (client_fd != sender_fd) {
-            send(client_fd, message.c_str(), message.size(), 0);
-        }
-    }
-}
-
-// Function to handle individual clients
 void handle_client(int client_socket) {
     char buffer[BUFFER_SIZE];
-    while (true) {
-        memset(buffer, 0, BUFFER_SIZE);
-        int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
-        if (bytes_received <= 0) {
-            break;
+    int bytes_received;
+
+    while ((bytes_received = recv(client_socket, buffer, sizeof(buffer), 0)) > 0) {
+        buffer[bytes_received] = '\0';  // Null-terminate the string
+        std::cout << "Message received: " << buffer << std::endl;
+
+        // Send the message to all connected clients
+        for (int client : clients) {
+            if (client != client_socket) {  // Don't send the message back to the sender
+                send(client, buffer, bytes_received, 0);
+            }
         }
-        std::string message = "Client " + std::to_string(client_socket) + ": " + std::string(buffer);
-        std::cout << message << std::endl;
-        broadcast(message, client_socket);
     }
 
-    // Remove client from list
-    {
-        std::lock_guard<std::mutex> lock(clients_mutex);
-        clients.erase(std::remove(clients.begin(), clients.end(), client_socket), clients.end());
+    if (bytes_received == 0) {
+        std::cout << "Client disconnected." << std::endl;
+    } else if (bytes_received < 0) {
+        std::cerr << "Error receiving message: " << strerror(errno) << std::endl;
     }
+
+    // Remove client from the list using the correct method
+    clients.erase(std::remove(clients.begin(), clients.end(), client_socket), clients.end());
+
     close(client_socket);
 }
 
 int main() {
-    int server_socket, client_socket;
-    sockaddr_in server_addr{}, client_addr{};
-    socklen_t addr_len = sizeof(client_addr);
+    int server_fd, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
 
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        std::cerr << "Failed to create socket\n";
-        return -1;
+    // Create socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
     }
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_addr.s_addr = INADDR_ANY;  // Bind to all available interfaces
     server_addr.sin_port = htons(PORT);
 
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        std::cerr << "Bind failed\n";
-        return -1;
+    // Bind socket
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
     }
 
-    if (listen(server_socket, 5) == -1) {
-        std::cerr << "Listen failed\n";
-        return -1;
+    // Listen for connections
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
     }
 
-    std::cout << "Server is running on port " << PORT << "...\n";
+    std::cout << "Chat Server running on port " << PORT << "..." << std::endl;
 
     while (true) {
-        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
-        if (client_socket == -1) {
-            std::cerr << "Failed to accept client\n";
-            continue;
+        // Accept incoming client connections
+        if ((client_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0) {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
         }
 
-        {
-            std::lock_guard<std::mutex> lock(clients_mutex);
-            clients.push_back(client_socket);
-        }
+        std::cout << "New client connected." << std::endl;
 
-        std::cout << "New client connected: " << client_socket << "\n";
+        clients.push_back(client_socket);
         std::thread(handle_client, client_socket).detach();
     }
 
-    close(server_socket);
+    close(server_fd);
     return 0;
 }
